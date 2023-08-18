2c093536c02e70b55638d711d04618d15fd75585
Jgarkoo <you@example.com>
2023-08-18 20:33:37 +0400

    event-binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventBindingComponent } from './event-binding/event-binding.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    EventBindingComponent,
   
   ],
   imports: [
